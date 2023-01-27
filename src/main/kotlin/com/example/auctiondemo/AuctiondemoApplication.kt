ca5de4b64d0e5867a1a4c3706dfee7d67126702b
Somrak Monpengpinij <somrakmonpengpinij@Somraks-MacBook-Pro.local>
2023-01-27 15:19:34 +0700

    init commit

---
+++ src/main/kotlin/com/example/auctiondemo/AuctiondemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.auctiondemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuctiondemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuctiondemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/auctiondemo/AuctiondemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.auctiondemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuctiondemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
