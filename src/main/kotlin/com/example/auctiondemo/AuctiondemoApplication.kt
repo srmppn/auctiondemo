d98a353a265172d30c6a8f50df69503ddd70aff2
Cartoonydesu <toon.pi45@gmail.com>
2023-02-08 14:25:28 +0700

    clean TEST class

--- src/main/kotlin/com/example/auctiondemo/AuctiondemoApplication.kt
+++ src/main/kotlin/com/example/auctiondemo/AuctiondemoApplication.kt
@@ -14,17 +14,4 @@ class AuctiondemoApplication
 fun main(args: Array<String>) {
 	runApplication<AuctiondemoApplication>(*args)
 
-//	val d3 = LocalDateTime.now().plusMinutes(20)
-//	println(d3)
-//
-//	val d4 = Instant.now().plusMillis(20*60*1000)
-//	println(d4)
-
-
-//	val d = Date(System.currentTimeMillis() + (60*60*1000))
-//	println(d)
-
-//	val d2 = Timestamp(System.currentTimeMillis() + (60*60*1000))
-//	println(d2)
-
 }
